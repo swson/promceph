cf859633fefbc17a6aca7f88a4cfd573ea35fc3e
Seung Woo Son <10246534+swson@users.noreply.github.com>
2023-04-15 22:15:01 -0400

    Update run-prombench-base.sh
    Delete obsolete version substitution script.

--- run-prombench-base.sh
+++ run-prombench-base.sh
@@ -4,7 +4,7 @@ source ./prombench-setup-base.sh
 echo "Cloning test-infra github ..."
 git clone https://github.com/prometheus/test-infra.git
 cd test-infra
-sed -i 's/6.3.0-beta1/7.5.2/g' prombench/manifests/cluster-infra/grafana_deployment.yaml
+#sed -i 's/6.3.0-beta1/7.5.2/g' prombench/manifests/cluster-infra/grafana_deployment.yaml
 make
 cd infra
 
