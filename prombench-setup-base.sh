c24e19a1a8e07bee70bd790b3cd9c682d34885af
Seung Woo Son <10246534+swson@users.noreply.github.com>
2023-04-15 23:14:52 -0400

    Update prombench-setup-base.sh
    Updated the script for installing kubectl

--- prombench-setup-base.sh
+++ prombench-setup-base.sh
@@ -57,6 +57,16 @@ echo "Install KIND ..."
 go install sigs.k8s.io/kind@v0.18.0 && kind create cluster
 
 echo "Install kubernetes ..."
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+## The following three lines were for old scripts
+#curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+#chmod +x ./kubectl
+#sudo mv ./kubectl /usr/local/bin/kubectl
+# Download the latest release
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# validate the binary
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+# Install kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# Test to ensure the version installed is up-to-date
+kubectl version --client
