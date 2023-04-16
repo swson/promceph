edf97a8d2be31a34ac26ade209e3969ee881df7e
Seung Woo Son <10246534+swson@users.noreply.github.com>
2023-04-16 00:14:42 -0400

    Update prombench-setup-base.sh
    Updated the step for installing docker engine

--- prombench-setup-base.sh
+++ prombench-setup-base.sh
@@ -5,20 +5,27 @@ sudo apt-get -y install \
      apt-transport-https \
      ca-certificates \
      curl \
-     gnupg-agent \
+     gnupg \
      software-properties-common
 
 echo "Add Docker’s official GPG key..."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo apt-key fingerprint 0EBFCD88
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo "Set up the stable repository ..."
-sudo add-apt-repository \
-     "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#sudo add-apt-repository \
+#     "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "Install docker engine ..."
 sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "Check docker version ..."
 sudo docker version
