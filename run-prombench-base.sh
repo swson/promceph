9f908a0bac2319889ec002cf62f7c361a0732d87
Seung Woo Son <10246534+swson@users.noreply.github.com>
2023-04-16 11:31:54 -0400

    Update run-prombench-base.sh
    Updated the script with the latest documentation

--- run-prombench-base.sh
+++ run-prombench-base.sh
@@ -6,22 +6,22 @@ git clone https://github.com/prometheus/test-infra.git
 cd test-infra
 #sed -i 's/6.3.0-beta1/7.5.2/g' prombench/manifests/cluster-infra/grafana_deployment.yaml
 make
-cd infra
+cd prombench
 
 echo "Set the following environment variables and deploy the cluster ..."
-sudo ./infra kind cluster create -v PR_NUMBER:$PR_NUMBER -v CLUSTER_NAME:$CLUSTER_NAME -f ../prombench/manifests/cluster_kind.yaml
+sudo ../infra/infra kind cluster create -v PR_NUMBER:$PR_NUMBER -v CLUSTER_NAME:$CLUSTER_NAME -f manifests/cluster_kind.yaml
 
 echo "Remove taint(node-role.kubernetes.io/master) from prombench-control-plane node for deploying nginx-ingress-controller ..."
 sudo kubectl taint nodes $CLUSTER_NAME-control-plane node-role.kubernetes.io/master-
 
 echo "Deploy the nginx-ingress-controller, Prometheus-Meta, Loki, Grafana, Alertmanager & Github Notifier ..."
-sudo ./infra kind resource apply -v CLUSTER_NAME:$CLUSTER_NAME -v DOMAIN_NAME:$DOMAIN_NAME \
+sudo ../infra/infra kind resource apply -v CLUSTER_NAME:$CLUSTER_NAME -v DOMAIN_NAME:$DOMAIN_NAME \
      -v GRAFANA_ADMIN_PASSWORD:$GRAFANA_ADMIN_PASSWORD \
      -v OAUTH_TOKEN="$(printf $OAUTH_TOKEN | base64 -w 0)" \
      -v WH_SECRET="$(printf $WH_SECRET | base64 -w 0)" \
      -v GITHUB_ORG:$GITHUB_ORG -v GITHUB_REPO:$GITHUB_REPO \
      -v SERVICEACCOUNT_CLIENT_EMAIL:$SERVICEACCOUNT_CLIENT_EMAIL \
-     -f ../prombench/manifests/cluster-infra
+     -f manifests/cluster-infra
 
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
@@ -42,10 +42,10 @@ sudo apt-get -y install firefox xauth jq
 echo "Start a benchmarking test manually ..."
 # Set the following environment variables
 # Deploy the k8s objects
-sudo ./infra kind resource apply -v CLUSTER_NAME:$CLUSTER_NAME \
+sudo ../infra/infra kind resource apply -v CLUSTER_NAME:$CLUSTER_NAME \
      -v PR_NUMBER:$PR_NUMBER -v RELEASE:$RELEASE -v DOMAIN_NAME:$DOMAIN_NAME \
      -v GITHUB_ORG:${GITHUB_ORG} -v GITHUB_REPO:${GITHUB_REPO} \
-     -f ../prombench/manifests/prombench/benchmark
+     -f manifests/prombench/benchmark
 
 # Deleting benchmark infra
-# sudo ./infra kind cluster delete -v PR_NUMBER:$PR_NUMBER -v CLUSTER_NAME:$CLUSTER_NAME -f ../prombench/manifests/cluster_kind.yaml
+# sudo ../infra/infra kind cluster delete -v PR_NUMBER:$PR_NUMBER -v CLUSTER_NAME:$CLUSTER_NAME -f manifests/cluster_kind.yaml
